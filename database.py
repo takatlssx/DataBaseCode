b92b08a0902127dc1e4d5ac39d8120cd9219f21e
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-10-28 15:24:30 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,21 @@
+import table
+
 class DataBase:
     
     def __init__(self,name,ini_file_path):
         self.name = name
         self.ini_file_path = ini_file_path
         self.ini_section_strs_list = []
+        
+        self.root_dir = ""
+        self.file_dir = ""
+        self.backup_dir = ""
+        
+        self.table_names = []
+        self.main_table_name = ""
+        self.relational_table_names = []
+        
+        self.tables = {}
     
     
     def initialize():
@@ -24,49 +36,58 @@ class DataBase:
         for section_strs in ini_section_strs_list:
             #このデータベースのセクションか確認
             if "[database]" in section_strs and f"name={self.name}" in section_strs:
-                pass
+                for line in section_strs:
+                    ky = "" if line == "[database]" else line.split("=")[0]
+                    vl = "" if line == "[database]" else line.split("=")[1]
+                    
+                    if ky == "root_dir":
+                        pass
+                    elif ky == "file_dir":
+                        pass
+                    elif ky == "backup_dir":
+                        pass
+                    elif ky == "member":
+                        pass
+                    elif ky =="main":
+                        pass
+                    elif ky == "relatinal":
+                        pass
+            #このデータベース所属のテーブルセクションか確認     
+            if "[table]" in section_strs and f"owner={self.name}" in section_strs:
+                for line in section_strs:
+                    ky = "" if line == "[table]" else line.split("=")[0]
+                    vl = "" if line == "[table]" else line.split("=")[1]
+                    
+                    tblName = ""
+                    colmuns = []
+                    types = []
+                    is_nullables = []
+                    aliases = []
+                    pk = ""
+                    
+                    if ky == "name":
+                        pass
+                    elif ky == "db_file_path":
+                        pass
+                    elif ky == "column":
+                        pass
+                    elif ky == "type":
+                        pass
+                    elif ky =="is_nullable":
+                        pass
+                    elif ky == "alias":
+                        pass
+                    elif ky == "primary_key":
+                        pass
+                    
+                    
             #このデータベースに所属するテーブルのセクションか確認
             if "[table]" in section_strs and f"owner={self.name}" in section_strs:
                 pass
                     
                 
                 
-        //設定取得
-        private void _initialize()
-        {
-            //iniを読み込み、文字列をIniSectionStrsList(List<List<string>>)に格納
-            using(StreamReader sr = new StreamReader(IniFilePath))
-            {
-                string line = "";
-                
-                //セクションの文字行を格納するリスト
-                List<string> sectionStrs = new List<string>();
-                
-                //現在処理中のセクション番号のカウンター
-                //IniSectionStrsListのインデックスとして使用
-                int sectionCount = -1;
-                
-                while((line = sr.ReadLine()) != null)
-                {                    
-                    //lineから空白文字を除去
-                    line = line.Replace(" ","").Replace("　","");
-                    
-                    //sectionラベル行なら、この前処理していたsectionCountをカウントアップし
-                    //IniSectionStrsListに新しいList<string>を追加
-                    if(line == "[database]" || line == "[table]")
-                    {
-                        sectionCount++;
-                        IniSectionStrsList.Add(new List<string>())
-                    }
-                    
-                    //1行の文字列を追加
-                    IniSectionStrsList[sectionCount].Add(line);
-                }
-            }
-            
-            //IniSectionStrsListをループし、このデータベースの情報を取得
-            foreach(List<string> sectionStrs in IniSectionStrsList)
-            {
+      
                 //このデータベースのセクションか確認
                 if(sectionStrs.Contains("[database]") && sectionStrs.Contains($"name={Name}"))
                 {
