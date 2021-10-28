1411ee9141c4356fc5b063f66988edd0ca687af0
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-10-28 15:54:26 +0900

    Create table.py

---
+++ table.py
@@ -0,0 +1,10 @@
+import column
+
+class Table:
+    
+    def __init__(self,name,db_file_path):
+        self.name = name
+        self.db_file_path = db_file_path
+    
+    def set_columns(self):
+        pass
