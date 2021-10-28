7c46b2f83fa217f28142ddbc5bf1f1a9e3e65dea
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-10-28 12:15:45 +0900

    Update Column.cs

--- Column.cs
+++ Column.cs
@@ -2,5 +2,19 @@ namespace DataBase
 {
     public class Column
     {
+        public string Name {get;}
+        public string Type {get;}
+        public bool InNullable {get;}
+        public string Alias {get;}
+        public int Index {get;}
+        
+        public Column(string name, string type, bool isNullable, string alias, int index)
+        {
+            Name = name;
+            Type = type;
+            IsNullable = isNullable;
+            Alias = alias;
+            Index = index;
+        }        
     }
 }
