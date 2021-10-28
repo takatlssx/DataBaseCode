22f196dd0a608d9db0ec46dd606ce79a81f5f588
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-10-28 11:14:09 +0900

    Update DataBase.cs

--- DataBase.cs
+++ DataBase.cs
@@ -3,9 +3,9 @@ namespace DataBase
     public class DataBase
     {
         public string Name {get; private set;}
-        public string IniFilePath; private set;}
+        public string IniFilePath {get; private set;}
     
-        
+        public List<List<string>> IniLineStrLists {get; private set;}
     
         public DataBase(string name, string iniFilePath)
         {
@@ -17,6 +17,18 @@ namespace DataBase
         private void _initialize()
         {
             //iniを読み込み、List<List<string>>に格納
+            using(StreamReader sr = new StreamReader(IniFilePath))
+            {
+                string line = "";
+                while((line = sr.ReadLine()) != null)
+                {
+                    line = line.Replace(" ","").Replace("　","");
+                    //section行か確認
+                    if(line == "[database]" || line == "[table]")
+                    {
+                    }
+                }
+            }
         }
     }
 }
