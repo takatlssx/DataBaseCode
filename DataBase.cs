e873265ca41c4cfc4af855c6d20a032ab1b5a378
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-10-28 11:09:41 +0900

    Update DataBase.cs

--- DataBase.cs
+++ DataBase.cs
@@ -2,9 +2,21 @@ namespace DataBase
 {
     public class DataBase
     {
-        public DataBase(string name, string iniFile)
+        public string Name {get; private set;}
+        public string IniFilePath; private set;}
+    
+        
+    
+        public DataBase(string name, string iniFilePath)
         {
-            
+            Name = name;
+            IniFilePath = iniFilePath;
+        }
+    
+        //設定取得
+        private void _initialize()
+        {
+            //iniを読み込み、List<List<string>>に格納
         }
     }
 }
